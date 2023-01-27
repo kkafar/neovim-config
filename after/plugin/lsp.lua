7ea92ebad97964a2cf4feab0202e2e848e52427e
Kacper Kafara <kacperkafara@gmail.com>
2023-01-27 09:27:08 +0100

    Improve lsp settings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,10 +23,10 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-Space>'] = cmp.mapping.complete(),
 })
 
-lsp.set_preferences({
-  sign_icons = {},
-  virtual_text = true,
-})
+-- lsp.set_preferences({
+--   sign_icons = {},
+--   virtual_text = true,
+-- })
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
@@ -36,17 +36,24 @@ lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.type_definition() end, opts)
+  vim.keymap.set("n", "<C-i>", function() vim.lsp.buf.implementation() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  -- vim.keymap.set("n", "<leader>vrf", function() vim.lsp.codelens.refresh() end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsb.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-vim.diagnostic.config({ virtual_text = true })
-
 lsp.setup()
+
+-- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v1.x/doc/md/lsp.md#configure-diagnostics
+vim.diagnostic.config({
+  virtual_text = true,
+})
+
