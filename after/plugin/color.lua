a97322d68d4615ad6649738945798cb19401db3b
Kacper Kafara <kacperkafara@gmail.com>
2023-04-17 22:18:28 +0200

    Enumerate all tokyo themes

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -3,10 +3,12 @@ vim.g.tokyonight_transparent_sidebar = false
 vim.g.tokyonight_transparent = true
 
 vim.opt.background = "light"
-vim.cmd("colorscheme tokyonight-day")
-
 -- vim.opt.background = "dark"
--- vim.cmd("colorscheme tokyonight-dark")
+--
+vim.cmd("colorscheme tokyonight-day")
+-- vim.cmd("colorscheme tokyonight-night")
+-- vim.cmd("colorscheme tokyonight-storm")
+-- vim.cmd("colorscheme tokyonight-moon")
 
 
 -- Configuration for rose-pine theme
