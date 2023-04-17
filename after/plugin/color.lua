8e4282b6b253cd677df2a226f00fb5a15a6c6d05
Kacper Kafara <kacperkafara@gmail.com>
2023-04-18 00:33:23 +0200

    Change default theme (#7)
    * Enumerate all tokyo themes
    
    * Set default theme to moon
    
    * Configure line numbers color

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -2,23 +2,20 @@
 vim.g.tokyonight_transparent_sidebar = false
 vim.g.tokyonight_transparent = true
 
-vim.opt.background = "light"
-vim.cmd("colorscheme tokyonight-day")
+-- vim.opt.background = "light"
+vim.opt.background = "dark"
+--
+-- vim.cmd("colorscheme tokyonight-day")
+-- vim.cmd("colorscheme tokyonight-night")
+-- vim.cmd("colorscheme tokyonight-storm")
+vim.cmd("colorscheme tokyonight-moon")
 
--- vim.opt.background = "dark"
--- vim.cmd("colorscheme tokyonight-dark")
 
-
--- Configuration for rose-pine theme
--- function ColorMyPencils(color)
---   color = color or "rose-pine"
---   vim.cmd.colorscheme(color)
--- 
---   -- 0 means "global space" --> so every window is affected
---   -- "Normal" means "vim" -- why? No one knows 
---   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- end
--- 
--- ColorMyPencils()
+local tokyotheme = require("tokyonight")
+tokyotheme.setup({
+  on_colors = function (colors)
+    colors.fg_gutter = "#b2b8cf"
+    colors.fg_dark = "#b2b8cf"
+  end
+})
 
