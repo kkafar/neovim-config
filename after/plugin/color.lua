2a7cc515e725ea17f2793540fd3e8d357f57f9a4
Kacper Kafara <kacperkafara@gmail.com>
2023-04-17 22:30:23 +0200

    Set default theme to moon

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -2,13 +2,13 @@
 vim.g.tokyonight_transparent_sidebar = false
 vim.g.tokyonight_transparent = true
 
-vim.opt.background = "light"
--- vim.opt.background = "dark"
+-- vim.opt.background = "light"
+vim.opt.background = "dark"
 --
-vim.cmd("colorscheme tokyonight-day")
+-- vim.cmd("colorscheme tokyonight-day")
 -- vim.cmd("colorscheme tokyonight-night")
 -- vim.cmd("colorscheme tokyonight-storm")
--- vim.cmd("colorscheme tokyonight-moon")
+vim.cmd("colorscheme tokyonight-moon")
 
 
 -- Configuration for rose-pine theme
