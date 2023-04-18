8a64a89a7481acc0df26d000c20f57c8aff16e08
Kacper Kafara <kacperkafara@gmail.com>
2023-04-18 02:05:35 +0200

    Change "show implementation" to <C-y> so that it does not collide with <C-i>
    It now collides with <C-y> but I'm rarely using this movement

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -55,7 +55,7 @@ lsp.on_attach(function(client, bufnr)
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "gD", function() vim.lsp.buf.type_definition() end, opts)
-  vim.keymap.set("n", "<C-i>", function() vim.lsp.buf.implementation() end, opts)
+  vim.keymap.set("n", "<C-y>", function() vim.lsp.buf.implementation() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
