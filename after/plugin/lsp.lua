68a0c1478a3e2b72935ccd0d21b1e9d2458ce0f4
Kacper Kafara <kacperkafara@gmail.com>
2023-05-22 09:12:54 +0200

    Add series of keybindings for LSP (& telescope) features + improve Rust setup (#21)
    * Improve keybindings for LSP
    
    TODO: Update documentation in howtos
    
    * Comment out "rust-tools" configuration in base dir
    
    * Enable diagnostics keybinding
    
    * Setup inline hint for Rust

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -32,6 +32,16 @@ lsp.configure("pylsp", {
   }
 })
 
+local rt = require("rust-tools");
+
+rt.setup({
+  inlay_hints = {
+    auto = true,
+    only_current_line = false,
+    show_parameter_hints = true,
+  }
+})
+
 local cmp = require("cmp")
 local cmp_select = { behaviour = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -54,20 +64,34 @@ lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gD", function() vim.lsp.buf.type_definition() end, opts)
+  vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+  vim.keymap.set("n", "<leader>D", function() vim.lsp.buf.type_definition() end, opts)
   vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vbs", function() vim.lsp.buf.document_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>bs", function() vim.lsp.buf.document_symbol() end, opts)
+
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+  vim.keymap.set({"n", "v"}, "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set({"n", "v"}, "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+
   vim.keymap.set("n", "<leader>vrf", function() vim.lsp.codelens.refresh() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("n", "<leader>vmt", function() vim.lsp.buf.format() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+  if client.name == "rust_analyzer" then
+    rt.inlay_hints.set()
+    rt.inlay_hints.enable()
+  end
 end)
 
 lsp.setup()
