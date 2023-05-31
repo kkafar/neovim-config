d3922965b30d93c06d35b46ff6374c4bbffa1cfb
Kacper Kafara <kacperkafara@gmail.com>
2023-05-31 21:23:55 +0200

    Attempt to add vcs to status line

--- lua/statusline/init.lua
+++ lua/statusline/init.lua
@@ -1,3 +1,19 @@
+local function vcs()
+  local branch_sign = ''
+  local git_info = vim.b.gitsigns_status_dict
+
+  if not git_info or git_info.head == '' then
+    return ''
+  end
+
+  local added = git_info.added and ('+' .. git_info.added .. ' ') or ''
+  local modified = git_info.changed and ('~' .. git_info.changed .. ' ') or ''
+  local removed = git_info.removed and ('-' .. git_info.removed .. ' ') or ''
+  local pad = ((added ~= '') or (removed ~= '') or (modified ~= '')) and ' ' or ''
+  local diff_str = string.format('%s%s%s%s', added, removed, modified, pad)
+  return string.format('%s%s %s ', diff_str, branch_sign, git_info.head)
+end
+
 local function status_line()
   local mode = "%-5{%v:lua.string.upper(v:lua.vim.fn.mode())%}"
   local file_name = "%-.16t"
