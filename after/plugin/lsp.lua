b8d69f8831c868bc56826209a824c2abde7eb9e4
Kacper Kafara <kacperkafara@gmail.com>
2023-06-04 20:35:56 +0200

    Try ge & gE keygindings for diagnostic navigation (#27)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -74,7 +74,9 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>bs", function() vim.lsp.buf.document_symbol() end, opts)
 
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "ge", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "gE", function() vim.diagnostic.goto_prev() end, opts)
 
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
