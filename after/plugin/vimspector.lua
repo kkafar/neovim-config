a9aeaa1b82154bb3c9a44b5dadfda6a0ff57db08
Kacper Kafara <kacperkafara@gmail.com>
2023-06-04 22:02:08 +0200

    Add more keybindings for vimspector

--- after/plugin/vimspector.lua
+++ after/plugin/vimspector.lua
@@ -7,6 +7,12 @@ vim.keymap.set("n", "<leader>dr", ":call vimspector#Reset()<CR>", opts)
 vim.keymap.set("n", "<leader>dc", ":call vimspector#Continue()<CR>", opts)
 vim.keymap.set("n", "<leader>dt", ":call vimspector#ToggleBreakpoint()<CR>", opts)
 vim.keymap.set("n", "<leader>dT", ":call vimspector#ClearBreakpoints()<CR>", opts)
+vim.keymap.set("n", "<leader>dbl", ":call vimspector#ListBreakpoints()<CR>", opts)
+vim.keymap.set("n", "<leader>dfk", ":call vimspector#UpFrame()<CR>", opts)
+vim.keymap.set("n", "<leader>dfj", ":call vimspector#DownFrame()<CR>", opts)
+vim.keymap.set("n", "<leader>dbn", ":call vimspector#JumpToNextBreakpoint()<CR>", opts)
+vim.keymap.set("n", "<leader>dbp", ":call vimspector#JumpToPreviousBreakpoint()<CR>", opts)
+
 
 vim.keymap.set("n", "<leader>dl", "<Plug>VimspectorStepOver", {})
 vim.keymap.set("n", "<leader>dj", "<Plug>VimspectorStepInto", {})
