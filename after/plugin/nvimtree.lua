efd6649a4ae22238ea55a51d0dd04abf3bae2925
Kacper Kafara <kacperkafara@gmail.com>
2023-06-22 23:32:10 +0200

    Improve nvimtree configuration

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -5,18 +5,32 @@ require("nvim-tree").setup({
   },
   renderer = {
     group_empty = true,
+    highlight_git = false,
     icons = {
       git_placement = "after",
-    }
+    },
+    indent_markers = {
+      enable = true,
+    },
   },
   view = {
     centralize_selection = true,
     cursorline = true,
     number = true,
     relativenumber = true,
+    float = {
+      enable = false,
+    }
   },
   git = {
     ignore = false, 
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+  modified = {
+    enable = true,
   }
 })
 
