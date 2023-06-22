85f6a178d2318b5eb7848c4a7f9834bd2159c717
Kacper Kafara <kacperkafara@gmail.com>
2023-06-22 22:25:24 +0200

    Set vim.opt.hlsearch to true

--- lua/kkafara/options.lua
+++ lua/kkafara/options.lua
@@ -8,7 +8,7 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.smartindent = true
