eb69a7a4c0ddd5bdb30c67f5d90eb8bc1b09e6a3
Kacper Kafara <kacperkafara@gmail.com>
2023-07-03 14:32:54 +0200

    Create new mappings & edit lsp config (#43)
    Edit mappings & lsp config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,10 +5,14 @@ return {
     build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects'
+    },
     config = function()
       require('nvim-treesitter.configs').setup({
         -- A list of parser names, or "all" (the four listed parsers should always be installed)
-        ensure_installed = { "c", "lua", "vim", "javascript", "typescript", "rust", "python" },
+        ensure_installed = { "c", "cpp", "tsx", "vimdoc", "vim", "lua", "vim", "javascript", "typescript", "rust",
+          "python" },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
@@ -17,37 +21,20 @@ return {
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
         auto_install = true,
 
-        -- List of parsers to ignore installing (for "all")
-        -- @kkafar: I do not want to ignore this
-        -- ignore_install = { "javascript" },
-
-        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+        -- TODO: Experiment with this feature
+        -- incremental_selection = {
+        --   enable = true,
+        --   keymaps = {
+        --     init_selection = '<c-space>',
+        --     node_incremental = '<c-space>',
+        --     scope_incremental = '<c-s>',
+        --     node_decremental = '<M-space>',
+        --   },
+        -- },
 
         highlight = {
           -- `false` will disable the whole extension
           enable = true,
-
-          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-          -- the name of the parser)
-          -- list of language that will be disabled
-          -- @kkafar: I want these enabled
-          -- disable = { "c", "rust" },
-
-          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-          -- disable = function(lang, buf)
-          --     local max_filesize = 100 * 1024 -- 100 KB
-          --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          --     if ok and stats and stats.size > max_filesize then
-          --         return true
-          --     end
-          -- end,
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
           additional_vim_regex_highlighting = false,
 
           autotag = {
@@ -57,11 +44,59 @@ return {
             enable_close_on_slash = true,
           }
         },
-
+        indent = {
+          enable = true,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>a'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>A'] = '@parameter.inner',
+            }
+          },
+        },
       })
     end,
   },
   {
     'nvim-treesitter/playground',
   },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  }
 }
