250bb3d61979429622b5845712966810fb5e2015
Kacper Kafara <kacperkafara@gmail.com>
2023-07-03 10:18:47 +0200

    Add / remove / set some options (vim.opt)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,9 @@
 -- Comment this option to have "thin cursor" in insert mode
+vim.g.mapleader = " "
+vim.g.maplocalleader = ' '
+
 vim.opt.guicursor = ""
+vim.opt.termguicolors = true
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -8,34 +12,29 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-
+vim.opt.breakindent = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = ' '
-
-
--- vim.opt.undodir = os.getenv("HOME") .. "./.vim/undodir"
--- vim.opt.undofile = true
-
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-
-vim.opt.updatetime = 50
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
 vim.opt.timeoutlen = 1000
 vim.opt.hidden = true
 vim.opt.cursorline = true
-
 vim.opt.icon = true
 vim.opt.ruler = true
+
+vim.opt.completeopt = 'menuone,noselect'
+
+-- vim.opt.undodir = os.getenv("HOME") .. "./.vim/undodir"
+-- vim.opt.undofile = true
 
 -- vim.cmd [[
 --   augroup YankHighlight
