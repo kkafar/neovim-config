27a548df524b513abec4d73cd25bc7f41eb585d1
Kacper Kafara <kacperkafara@gmail.com>
2023-07-03 10:13:06 +0200

    Move remap & options to config module

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+require("config")
 require("plugins")
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,3 @@
-require("plugins.options")
 require("plugins.packer")
 require("plugins.lsp")
-require("plugins.remap")
 
