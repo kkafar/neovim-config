ae8b5cfcf6742723f6e4f4cb51fb4cede9490ad1
Kacper Kafara <kacperkafara@gmail.com>
2023-07-03 09:21:47 +0200

    Playaround with options of rust filetype plugin (#38)
    Modify options of rust filetype plugin

---
+++ after/ftplugin/rust.lua
@@ -0,0 +1,15 @@
+-- See :h g:rust
+
+-- vim.cmd([[
+--   let g:rust_recommended_style = 0
+--   let g:rustfmt_autosave = 1
+--   let g:rustfmt_fail_silently = 1
+-- ]])
+--
+vim.g.rust_recommended_style = false
+vim.g.rustfmt_autosave = false
+vim.g.rustfmt_fail_silently = false
+
+-- vim.opt.tabstop = 2
+-- vim.opt.softtabstop = 2
+-- vim.opt.shiftwidth = 2
