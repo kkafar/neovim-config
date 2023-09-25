2fe24c63486d3f0406665f8129220c5467b9de72
Kacper Kafara <kacperkafara@gmail.com>
2023-09-25 09:16:52 +0200

    Move vim-fugitive configuration

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,9 @@
 return {
-  'tpope/vim-fugitive',
+  {
+    'tpope/vim-fugitive',
+    lazy = true,
+    config = function ()
+      vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+    end
+  },
 }
