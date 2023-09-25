36b6fc74c29abba7f8707f5eeedad2f5a8adec69
Kacper Kafara <kacperkafara@gmail.com>
2023-09-25 09:14:35 +0200

    Move floatterm configuration to plugins

--- after/plugin/floaterm.lua
+++
@@ -1,7 +0,0 @@
-local opts = { remap = false }
-
-vim.keymap.set('n', '<leader>tn', ':FloatermNew --disposable<CR>', opts)
-vim.keymap.set('n', '<leader>tl', ':FloatermNext<CR>', opts)
-vim.keymap.set('n', '<leader>th', ':FloatermPrev<CR>', opts)
-vim.keymap.set('n', '<leader>tt', ':FloatermToggle<CR>', opts)
-vim.keymap.set('t', '<leader>tc', 'exit<CR>', opts)
