5544f9fb19c491d3e522388946c7032498a35602
Kacper Kafara <kacperkafara@gmail.com>
2024-12-05 00:23:01 +0100

    Update Lazy initialization code

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,4 +15,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins") -- load plugins
+-- load plugins
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+})
+
