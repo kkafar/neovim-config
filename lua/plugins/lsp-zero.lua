73177f249a98f61625e3f50e35fc687185d4a264
Kacper Kafara <kacperkafara@gmail.com>
2025-02-08 18:57:25 +0100

    Format some code

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -127,8 +127,10 @@ return {
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 
-        vim.keymap.set("n", "<leader>vmt", function() vim.lsp.buf.format({ formatting_options = CommonFormattingOptions.formatting_options }) end, opts)
-        vim.keymap.set("n", "<F3>", function() vim.lsp.buf.format({ formatting_options = CommonFormattingOptions.formatting_options }) end, opts)
+        vim.keymap.set("n", "<leader>vmt",
+          function() vim.lsp.buf.format({ formatting_options = CommonFormattingOptions.formatting_options }) end, opts)
+        vim.keymap.set("n", "<F3>",
+          function() vim.lsp.buf.format({ formatting_options = CommonFormattingOptions.formatting_options }) end, opts)
 
         -- if client.name == "rust_analyzer" then
         --   local rt = require("rust-tools")
