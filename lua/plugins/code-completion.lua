ebf5212d2ddcc9e1cf8e3324a258d0f6160be847
Kacper Kafara <kacperkafara@gmail.com>
2025-02-10 21:45:13 +0100

    Improve blink.cmp experience

--- lua/plugins/code-completion.lua
+++ lua/plugins/code-completion.lua
@@ -3,7 +3,9 @@ return {
     'saghen/blink.cmp',
     dependencies = {
       { 'rafamadriz/friendly-snippets' },
-      { 'saghen/blink.compat' } },
+      { 'saghen/blink.compat' },
+      { 'folke/lazydev.nvim' }
+    },
     -- use a release tag to download pre-built binaries
     version = '*',
     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
@@ -19,18 +21,14 @@ return {
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- See the full "keymap" documentation for information on defining your own keymap.
       keymap = {
-        preset = 'default',
+        preset = 'enter',
         -- Can't use Enter for completion. Even when combined with selection.preselect function that
         -- should disable preselect in cmdline, hitting enter will accept suggestion and not execute command!
         -- This makes it impossible to execute any command, e.g. save the text using :w. Let's use <C-y> for a while...
-        -- ['<CR>'] = { 'select_and_accept' }
+        ['<CR>'] = { 'accept', 'fallback' },
       },
 
       appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- Will be removed in a future release
-        use_nvim_cmp_as_default = true,
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono'
@@ -38,23 +36,50 @@ return {
 
       completion = {
         list = {
+
           selection = {
             preselect = function(ctx)
+              return ctx.mode ~= 'cmdline'
+            end,
+            auto_insert = function(ctx)
               return ctx.mode ~= 'cmdline'
             end
           }
         },
+
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 300,
+          window = {
+            max_height = 120, -- 80 by default
+            border = 'rounded'
+          },
           -- might impact performance
           treesitter_highlighting = true,
+        },
+
+        accept = {
+          create_undo_point = true,
+          auto_brackets = {
+            enabled = true,
+          }
+        },
+
+        menu = {
+          max_height = 20,
+          border = 'rounded',
         }
       },
+
       signature = {
         enabled = true,
       },
 
+      fuzzy = {
+        max_typos = function(keyword)
+          return math.min(math.floor(#keyword / 4), 2)
+        end
+      },
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       -- 'lazydev' source comes from lazydev plugin by folke
